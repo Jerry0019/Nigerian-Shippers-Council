02de4ff86d9edb3b8720e919e6b7bc5f07459d64
Jerry0019 <157806653+Jerry0019@users.noreply.github.com>
2025-03-16 08:59:05 +0100

    Updated backend

--- nsc-map-backend/server.js
+++ nsc-map-backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 
@@ -8,6 +9,14 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+
+// Serve static files from the public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve index.html for the root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use('/api/users', userRoutes);
 
